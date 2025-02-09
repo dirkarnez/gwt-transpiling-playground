22b28d58690f37c6a994155237ff52815a47d8a3
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-09 18:18:08 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -7,7 +7,7 @@ C:\hostedtoolcache\windows\Java_Temurin-Hotspot_jdk\21.0.6-7.0\x64\bin;
 @REM webAppCreator.cmd
 @REM @REM  
 @REM java -cp gwt-dev.jar com.google.gwt.dev.Compiler compile com.example.MyEntryPoint
-java -classpath "%~dp0;%USERPROFILE%\Downloads\gwt-2.12.1\*" com.google.gwt.dev.Compiler -saveSourceOutput -optimize -saveSource -logLevel ALL -includeJsInteropExports client.* -generateJsInteropExports -deploy deployment mymodule &&^
+java -classpath "%~dp0;%USERPROFILE%\Downloads\gwt-2.12.1\gwt-2.12.1\*" com.google.gwt.dev.Compiler -saveSourceOutput -optimize -saveSource -logLevel ALL -includeJsInteropExports client.* -generateJsInteropExports -deploy deployment mymodule &&^
 echo ok
 
 
